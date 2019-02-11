78f78dfb4fed893c7848f5ac302e4f67fbd28b7a
David Lineman <dline215@gmail.com>
2019-02-11 21:58:11 +0000

    add asssociations between articles and user and add debugger view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190211200322) do
+ActiveRecord::Schema.define(version: 20190211213628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190211200322) do
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
