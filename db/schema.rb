63baab8e40ae883a168925b9e731278631db3065
David Lineman <dline215@gmail.com>
2019-02-14 21:36:36 +0000

    added categories controller and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190214183046) do
+ActiveRecord::Schema.define(version: 20190214205140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20190214183046) do
     t.datetime "created_at"
     t.integer  "user_id"
     t.datetime "updated_at"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
