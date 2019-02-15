80c99653b1ad6bd210d481708c3f4727f9288528
David Lineman <dline215@gmail.com>
2019-02-15 19:21:20 +0000

    add catgeory and list tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -5,14 +5,26 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
   test "get new category form and create new category" do 
     sign_in_as(@user, "password")
-    # get new_category_path
-    # assert_template 'categories/new'
-    # assert_difference 'Category.count', 1 do
-    #   post categories_path, params: { category: {name: "sports"} } 
-    #   follow_redirect!
-    # end
-    # assert_template 'category/index'
-    # assert_match "sports", response.body
+    get new_category_path
+    assert_template 'categories/new'
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: {name: "sports"} } 
+      follow_redirect!
+    end
+    assert_template 'category/index'
+    assert_match "sports", response.body
+  end
+
+  test "invalid category submission results in failure" do 
+    sign_in_as(@user, "password")
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name: " "} } 
+    end
+    assert_template 'category/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
   end
 
 end
