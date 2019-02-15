cb8eed751302523f6efce6a73640247805826bac
David Lineman <dline215@gmail.com>
2019-02-15 20:27:50 +0000

    add admin capabilities

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -13,6 +13,7 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do 
+    sign_in_as(@user, "password")
     get new_category_path
     assert_response :success
   end
@@ -20,5 +21,12 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do 
     get categories_path(@category), params: (@category.id)
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do 
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: {name:"sports"} }
+    end
+    assert_redirected_to categories_path
   end
 end
